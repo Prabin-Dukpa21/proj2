b9e4dcc35741183a977c2688faf4d725b2f5a646
Prabin-Dukpa21 <prabindukpa891869@gmail.com>
2022-09-13 15:59:45 +0530

    ko

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prabin:pd891869@cluster0.38akqf8.mongodb.net/prabin-db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
